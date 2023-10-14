376e800b44871531ed039584c9929ffaab697615
Selima <benturkiaselima14@gmail.com>
2023-10-14 17:06:33 +0200

    feat: profile page

---
+++ lib/views/widgets/profile_page.dart
@@ -0,0 +1,66 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.amber,
+          ),
+          Container(
+            child: Column(
+              children: [
+                SizedBox(height: 50,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(onPressed: () {
+                      Navigator.pop(context);
+                          }
+                    , icon: Icon(Icons.arrow_back_ios),
+                    padding: EdgeInsets.all(38),),
+                    IconButton(onPressed: () {},
+                        icon: Icon(Icons.close),
+                        padding: EdgeInsets.all(38),
+                        iconSize: 30,)
+                  ],
+                ),
+                Container(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/profile_icon.png'),
+                  backgroundColor: Colors.amber,
+                  radius: 100,
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(50),
+                        topLeft: Radius.circular(50))
+                  ),
+
+                )
+
+
+              ],
+            ),
+          )
+        ],
+      ),
+
+
+    );
+  }
+}
