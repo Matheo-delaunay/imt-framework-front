1a7a50c1668022684ba7a33e336d66f6e1eeefca
Selima <benturkiaselima14@gmail.com>
2023-10-15 14:12:07 +0200

    fix: nav bar icons size

--- lib/views/home_nav_bar.dart
+++ lib/views/home_nav_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:imt_framework_front/main.dart';
 import 'package:provider/provider.dart';
-import 'package:provider/provider.dart';
 
 class HomeNavBar extends StatelessWidget {
   @override
@@ -23,20 +22,21 @@ class HomeNavBar extends StatelessWidget {
                     selectedIcon: Icon(
                       Icons.home,
                       color: Colors.amber,
+                      size: 35,
                     ),
-                    icon: Icon(Icons.home_outlined),
+                    icon: Icon(Icons.home_outlined, size: 35, weight: 10),
                     label: 'Home',
                   ),
                   NavigationDestination(
                     selectedIcon: Icon(Icons.favorite,
-                        color: Colors.amber),
-                    icon: Icon(Icons.favorite_border),
+                        color: Colors.amber, size: 35,),
+                    icon: Icon(Icons.favorite_border, size: 35,),
                     label: 'Business',
                   ),
                   NavigationDestination(
                     selectedIcon: Icon(Icons.shopping_cart,
-                        color: Colors.amber),
-                    icon: Icon(Icons.shopping_cart_outlined),
+                        color: Colors.amber, size: 35,),
+                    icon: Icon(Icons.shopping_cart_outlined, size: 35,),
                     label: 'School',
                   ),
                 ],
