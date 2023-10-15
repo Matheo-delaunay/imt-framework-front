e8b6bc262a4a0c8e604285160fa6a529f4971aff
Selima <benturkiaselima14@gmail.com>
2023-10-15 20:04:49 +0200

    fix: move PageSeparator widget

--- lib/views/widgets/textfieldwidget.dart
+++ lib/views/widgets/textfieldwidget.dart
@@ -36,23 +36,25 @@ class _TextFieldWidgetState extends State<TextFieldWidget> {
   }
 
   @override
-  Widget build(BuildContext context) => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        widget.label,
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-      ),
-      const SizedBox(height: 8),
-      TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
+  Widget build(BuildContext context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.label,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
         ),
-        maxLines: widget.maxLines,
-      ),
-    ],
-  );
+        const SizedBox(height: 8),
+        TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          maxLines: widget.maxLines,
+        ),
+      ],
+    );
+  }
 }
