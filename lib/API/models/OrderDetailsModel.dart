22fe0f042d29281634d49eaa8636beff28e8a29d
naudvard <nathan.audvard@adeo.com>
2023-10-18 10:31:56 +0200

    fix endpoints

--- lib/API/models/OrderDetailsModel.dart
+++ lib/API/models/OrderDetailsModel.dart
@@ -1,30 +1,24 @@
 import 'dart:convert';
 
-import 'package:imt_framework_front/API/models/UserModel.dart';
-
 OrderDetailsModel orderDetailsModelFromJson(String str) => OrderDetailsModel.fromJson(json.decode(str));
 
 class OrderDetailsModel {
   int id;
-  UserModel user;
-  String address;
   double price;
   int date;
-  List<OrderLine> orderLines;
+  List<String> orderLines;
 
   OrderDetailsModel({
     required this.id,
-    required this.userId,
     required this.price,
     required this.date,
-    required this.isFinished,
+    required this.orderLines,
   });
 
   factory OrderDetailsModel.fromJson(Map<String, dynamic> json) => OrderDetailsModel(
     id: json["id"],
-    userId: json["userId"],
     price: json["price"],
     date: json["date"],
-    isFinished: json["isFinished"],
+    orderLines: json["orderLines"],
   );
 }
