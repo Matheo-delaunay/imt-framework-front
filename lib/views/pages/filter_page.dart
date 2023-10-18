892cba112d55c552834b66446e4a55ced55de07e
Matheo Delaunay <matheo.delaunay@adeo.com>
2023-10-18 05:29:23 +0200

    Feat: add filter

---
+++ lib/views/pages/filter_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:imt_framework_front/main.dart';
+import 'package:imt_framework_front/views/widgets/appbar/appBar.dart';
+import 'package:provider/provider.dart';
+
+class FilterPage extends StatelessWidget {
+  const FilterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return Scaffold(
+      body: Column(
+        children: [
+          TopAppBar(arrowVisible: true, heartVisible: false, title: 'Filter'),
+          Expanded(child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 30.0),
+                child: Text("Alergens",style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),),
+              ),
+              Wrap(
+                  spacing: 10,
+                  children:
+                  appState.filter.entries.map<FilterChip>((entry) =>
+                      FilterChip(
+                        label: Text(entry.key),
+                        onSelected: (bool) {appState.changeChipState(bool, entry.key);},
+                        selectedColor: Colors.amber,
+                        selected: entry.value,
+                      )).toList()
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
